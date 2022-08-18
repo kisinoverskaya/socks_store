cbe0456fdb837eddb34a48f649dbf967fe87eadf
Vadim <vadimir29@gmail.com>
2022-08-18 12:40:12 +0300

    add client script

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,24 @@
+const wraper = document.querySelector(".wraper");
+
+wraper.addEventListener("click", (e) => {
+  const sockImg = document.querySelector(".sock-img");
+  const sockImgSrc = document.querySelector(".sock-img-src");
+  if (e.target.className === "color-item") {
+    sockImg.style.backgroundColor = e.target.dataset.color;
+  }
+  if (e.target.className === "src-item-block") {
+    sockImg.style.backgroundImage = `url('../img/${e.target.dataset.src}.png')`;
+  }
+  if (e.target.className === "btn-buy") {
+    console.log("тут будет фетч");
+    console.log(sockImg.dataset.src);
+    const body = JSON.stringify({
+      color: sockImg.style.backgroundColor,
+    });
+    console.log(body);
+  }
+});
+
 // find modal
 const register = document.querySelector(".register");
 register.addEventListener("click", (e) => {
