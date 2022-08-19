196dde9f345ddda0590ba959a2736120e869b596
kisinoverskaya <kisinoverskaya94@gmail.com>
2022-08-19 10:12:05 +0300

    db fixed

--- db/models/Basket.js
+++ db/models/Basket.js
@@ -18,6 +18,7 @@ module.exports = (sequelize, DataTypes) => {
       src: DataTypes.TEXT,
       price: DataTypes.INTEGER,
       userId: DataTypes.INTEGER,
+      count: DataTypes.INTEGER,
     },
     {
       sequelize,
