57c79794e664a37af5241515678962aba53c5148
Vadim <vadimir29@gmail.com>
2022-08-19 13:07:22 +0300

    fix basket

--- views/components/Footer.jsx
+++ views/components/Footer.jsx
@@ -1,7 +1,13 @@
 const React = require("react");
 
 const Footer = () => {
-  return <h1>footer</h1>;
+  return (
+    <div className="footer">
+      <h2>footer</h2>
+      <hr />
+      <h2>23</h2>
+    </div>
+  );
 };
 
 module.exports = Footer;
